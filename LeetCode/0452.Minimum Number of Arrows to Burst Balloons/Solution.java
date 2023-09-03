e0fb91e898d17033e940701fdd41b0a6b3df60f3
Nazmul Karim Nabil <64605226+nazmulnabil@users.noreply.github.com>
2023-09-03 09:42:28 +0000

    solved as activity selection pattern

---
+++ LeetCode/0452.Minimum Number of Arrows to Burst Balloons/Solution.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        int c=1;
+        if(points.length==1)
+        return 1;  
+       Arrays.sort(points, Comparator.comparingInt(a->a[1]));
+      
+       int end=points[0][1];
+
+       for(int i=1;i<points.length;i++){
+           
+        if(points[i][0] > end ){
+               c++;
+               end=points[i][1];
+        }
+       }
+    
+        return c;
+    }
+}
