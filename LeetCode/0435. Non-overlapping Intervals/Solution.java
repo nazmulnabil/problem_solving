61d2fbc136d2913e1f21b28eb6e66d324d5950bd
Nazmul Karim Nabil <64605226+nazmulnabil@users.noreply.github.com>
2023-09-04 03:32:40 +0000

    solved as activity selection pattern

---
+++ LeetCode/0435. Non-overlapping Intervals/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals.length==1)
+            return 0;
+       Arrays.sort(intervals, Comparator.comparingInt(a->a[1]));
+        
+        int c=1;
+        int end=intervals[0][1];
+        
+        for(int i=0;i<intervals.length;i++){
+           if(intervals[i][0]>=end){
+               c++;
+               end=intervals[i][1];
+           } 
+            
+            
+        }
+        return intervals.length-c;
+        
+    }
+}
