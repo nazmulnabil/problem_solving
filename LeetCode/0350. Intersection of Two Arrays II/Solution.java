568d5ae1eae71adfb2e0a44e402d6d82adabbffa
Nazmul Karim Nabil <64605226+nazmulnabil@users.noreply.github.com>
2023-09-14 05:35:33 +0000

    two pointers approach

--- LeetCode/0350. Intersection of Two Arrays II/Solution.java
+++ LeetCode/0350. Intersection of Two Arrays II/Solution.java
@@ -1,42 +1,35 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Set<Integer> set =new HashSet<>();
-        int len1=nums1.length;
-        int len2=nums2.length;
-        int lenMin=Math.min(len1, len2);
-        int lenMax=Math.max(len1, len2);
+        int left=0,right=0;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list=new ArrayList<>();
         
-        List<Integer> list =new ArrayList<>();
-        List<Integer> inersectionlist =new ArrayList<>();
+        while(left<nums1.length && right<nums2.length){
+            
+             if(nums1[left]==nums2[right]){
+                list.add(nums1[left]);
+                left++;
+                right++;
+             }
+             else if(nums1[left]<nums2[right]){
+                left++;
+             }
+             else if(nums2[right]<nums1[left]){
+                right++;
+             }
+         }
 
-        for(int i=0;i<len1;i++){
-            set.add(nums1[i]);
-        }
-        for(int j=0;j<len2;j++){
-            if(set.contains(nums2))
-            list.add(nums2[j]);
-        }
-        if(len1>len2){
-            for(int k=0;k<len1;k++){
-                if(list.contains(nums1[k]))
-                inersectionlist.add(nums1[k]);
-            }
-        }
-        else{
-            for(int k=0;k<len2;k++){
-                if(list.contains(nums2[k]))
-                inersectionlist.add(nums2[k]);
-            }
-        }
-       int[] res=new intinersectionlist.size();
-       for(int l=0;l<intinersectionlist.size();l++){
-        res[i]=intinersectionlist.get(l);
-       }
+        int[] res=new int[list.size()];
 
-    
+        for(int i=0;i<res.length;i++){
+            res[i]=list.get(i);
+        }
+        
         return res;
     }
 }
